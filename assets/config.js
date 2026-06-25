127aba3290692c2519b0df662e66115a041b40e0
ramonpedrosa1407 <pradopedrosa@gmail.com>
2026-06-25 17:25:34 -0300

    Config: credenciais da plataforma

---
+++ assets/config.js
@@ -0,0 +1,7 @@
+// Configuração da plataforma Jornada Espírita
+window.JE_CONFIG = {
+  supa_url: 'https://sdwxwdqnfukxeofscmpd' + '.supabase.co',
+  supa_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
+    '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkd3h3ZHFuZnVreGVvZnNjbXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NjAwNjQsImV4cCI6MjA5NzUzNjA2NH0' +
+    '.S3UmOhXEhNv3-1qYWbN8NUFU9alxX0OZZUSAfU6TZuo'
+};
