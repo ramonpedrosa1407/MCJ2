ea2839f439b6325608e56681ed4aa7cfaa74e578
ramonpedrosa1407 <pradopedrosa@gmail.com>
2026-08-24 17:55:02 -0300

    Create gev-config.js

---
+++ GEV/assets/gev-config.js
@@ -0,0 +1,7 @@
+// Grupo Evoluir — configuração do módulo
+window.GEV_CONFIG = {
+  supabaseUrl: 'https://sdwxwdqnfukxeofscmpd.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkd3h3ZHFuZnVreGVvZnNjbXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NjAwNjQsImV4cCI6MjA5NzUzNjA2NH0.S3UmOhXEhNv3-1qYWbN8NUFU9alxX0OZZUSAfU6TZuo',
+  bucketAudio: 'gev-audio',
+  bucketMateriais: 'gev-materiais'
+};
